0cb6dcd6de5570c6322874eb9dbe11c7563314f2
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-01 22:14:34 +0800

    in-place shuffle reference methods added

--- 214_singleriffleshuffle.rb
+++ 214_singleriffleshuffle.rb
@@ -7,12 +7,6 @@ def get_random(floor, ceiling)
   random = rand(floor..ceiling)
 end
 
-# get_random(1,100)
-
-# APPROACH:
-# Choose a first random item to move to the first index
-# Choose a second random item to move to the second index
-
 # ANSWER:
 
 def answer_get_random(floor, ceiling)
@@ -21,19 +15,18 @@ end
 
 def answer_shuffle(array)
   return array unless array.length > 1 # if it's 1 or 0 items, just return
-  last_index_in_array = array.length - 1
-  (0..array.length - 2).each do |index_we_are_choosing_for|
+  last_index = array.length - 1
+  (0..array.length - 2).each do |chosen_index|
     # choose a random not-yet-placed item to place there (could also be the item
     # currently in that spot) which must be an item AFTER the current item,
     # because the stuff before has all already been placed
-    random_choice_index = answer_get_random(index_we_are_choosing_for, last_index_in_array)
+    random_index = answer_get_random(chosen_index, last_index)
 
     # place our random choice in the spot by swapping
-    if random_choice_index != index_we_are_choosing_for
-      array[index_we_are_choosing_for], array[random_choice_index] =
-        array[random_choice_index], array[index_we_are_choosing_for]
+    if random_index != chosen_index
+      array[chosen_index], array[random_index] = array[random_index], array[chosen_index]
     end
   end
 end
 
-answer_get_random(1..100)
+answer_get_random(1,100)
