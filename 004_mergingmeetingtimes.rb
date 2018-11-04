e89ac2cba0f8c60dc7ad7f57bbcdcfc46e2fb591
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-04 20:04:11 +0800

    added sample answer for reference

--- 004_mergingmeetingtimes.rb
+++ 004_mergingmeetingtimes.rb
@@ -16,4 +16,25 @@ def merge_ranges(array)
 end
 
 array = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
-merge_ranges(array)
+merge_ranges(array) # should return [[0, 1], [3, 8], [9, 12]]
+
+# ANSWER:
+
+def answer(meetings)
+  sorted_meetings = meetings.sort # sort by start time
+  merged_meetings = [sorted_meetings[0]] # initialise merged meetings with the earliest meeting
+
+  sorted_meetings[1..-1].each do |current_start, current_end|
+    last_meeting_start, last_meeting_end = merged_meetings[-1]
+    # if the current meeting overlaps with the last merged meeting, use the later end time of the two
+    if current_start <= last_meeting_end
+      merged_meetings[-1] = [ last_meeting_start, [last_meeting_end, current_end].max ]
+    else
+      merged_meetings << [current_start, current_end]
+    end
+  end
+  p merged_meetings
+end
+
+meetings = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+answer(meetings)
