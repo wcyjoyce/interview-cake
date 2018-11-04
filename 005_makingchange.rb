4d940ec1963d235d55e6bc86d246f3506afe5556
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-04 20:14:20 +0800

    TODO added

---
+++ 005_makingchange.rb
@@ -0,0 +1,13 @@
+# TODO:
+# Write a function that computes the number of ways to make the amount of money
+# with coins of the available denominations, given (1) an amount of money, and
+# (2) a list of coin denominations.
+
+# EXAMPLE:
+# amount = 4 (4¢)
+# denominations = [1,2,3] (1¢, 2¢ and 3¢)
+
+# 1¢, 1¢, 1¢, 1¢
+# 1¢, 1¢, 2¢
+# 1¢, 3¢
+# 2¢, 2¢
