084f42fcedbc44a2c5492428541cd3a4277db644
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-04 19:16:51 +0800

    removed example

--- 003_highestproductofthree.rb
+++ 003_highestproductofthree.rb
@@ -1,10 +1,6 @@
 # TODO:
 # Given a list of integers, find the highest product you can get from three of the integers.
 
-# Given: [-10, -10, 1, 3, 2]
-# Your method should return: 300
-# By calculating: -10 * -10 * 3
-
 def highest_product_of_three(array)
   negative_count = array.select { |value| value < 0 }
   array.map! { |value| value.abs }.sort! if negative_count.length.even?
