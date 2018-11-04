1fd85df5e11a0119576a6bb6ab85517ba8c59a5f
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-04 19:58:57 +0800

    problem #004 done

---
+++ 004_mergingmeetingtimes.rb
@@ -0,0 +1,23 @@
+# TODO:
+# Write a method merge_ranges() that takes an array of multiple meeting time
+# ranges and returns an array of condensed ranges.
+
+# For example:
+# Given:                     [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+# Your method should return: [0, 1], [3, 8], [9, 12]]
+
+def merge_ranges(array)
+  array.sort!
+  range = []
+  range << array.first
+  for i in (0..array.length - 1)
+    if array[i - 1].last < array[i].first
+      merged_times = [(array[i] + array[i + 1]).first, (array[i] + array[i + 1]).last]
+      range << merged_times
+    end
+  end
+  p range
+end
+
+array = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+merge_ranges(array)
