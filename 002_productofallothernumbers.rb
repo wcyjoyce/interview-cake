5f1ab4f8ab29b597d9b1d66de526b39bf0a9f15d
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-04 08:31:56 +0800

    added answer for reference

--- 002_productofallothernumbers.rb
+++ 002_productofallothernumbers.rb
@@ -24,3 +24,34 @@ end
 array = [1, 7, 3, 4]
 get_products_of_all_ints_except_at_index(array)
 
+# ANSWER:
+
+def answer(array)
+  raise IndexError, "At least 2 numbers are required" if array.length < 2
+
+  # for each integer, we find the product of all integers BEFORE it (and update total product)
+  products_of_all_ints_except_at_index = []
+  product_so_far = 1
+  i = 0
+
+  while i < array.length
+    products_of_all_ints_except_at_index[i] = product_so_far
+    product_so_far *= array[i]
+    i += 1
+  end
+
+  # for each integer, we find the product of all integers AFTER it (and adding this to total product)
+  product_so_far = 1
+  i = array.length - 1
+
+  while i >= 0
+    products_of_all_ints_except_at_index[i] *= product_so_far
+    product_so_far *= array[i]
+    i -= 1
+  end
+
+  p products_of_all_ints_except_at_index
+end
+
+array = [1, 7, 3, 4]
+answer(array)
