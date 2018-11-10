982f0c7e7b6eb3697d7cadec1202bf6bb985229a
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-10 15:46:51 +0800

    method done

--- 006_rectangularlove.rb
+++ 006_rectangularlove.rb
@@ -4,70 +4,32 @@
 def overlap(coordinate_1, range_1, coordinate_2, range_2)
   start_point = [coordinate_1, coordinate_2].max
   end_point = [(coordinate_1 + range_1), (coordinate_2 + range_2)].min
-  start_point < end_point ? range = start_point - end_point : nil
-  p range
+  if start_point < end_point
+    overlap_range = end_point - start_point
+    [start_point, overlap_range]
+  else
+    return [nil, nil]
+  end
 end
 
-overlap(1,6,5,3) # x
-overlap(1,3,2,3) # y
-overlap(1,1,4,1)
-overlap(1,1,4,4)
-
 def intersect(rectangle_1, rectangle_2)
-  width_endpoint_1 = rectangle_1[:left_x] + rectangle_1[:width]
-  x_overlap = width_endpoint_1 - rectangle_2[:left_x] if width_endpoint_1 >= rectangle_2[:left_x]
-  height_endpoint_1 = rectangle_1[:bottom_y] + rectangle_1[:height]
-  y_overlap = height_endpoint_1 - rectangle_2[:bottom_y] if height_endpoint_1 >= rectangle_2[:bottom_y]
-  x_coordinate = width_endpoint_1 - x_overlap
-  y_coordinate = height_endpoint_1 - y_overlap
-
-  overlap = {
-    left_x: x_coordinate, # 5
-    bottom_y: y_coordinate, # 2
-    width: x_overlap, # 2
-    height: y_overlap, # 2
-  }
+  x_coordinate, x_overlap = overlap(rectangle_1[:left_x], rectangle_1[:width], rectangle_2[:left_x], rectangle_2[:width])
+  y_coordinate, y_overlap = overlap(rectangle_1[:bottom_y], rectangle_1[:height], rectangle_2[:bottom_y], rectangle_2[:height])
+  if x_overlap = nil || y_overlap == nil
+    overlap = {
+      left_x: nil,
+      bottom_y: nil,
+      width: nil,
+      height: nil
+    }
+  else
+    overlap = {
+    left_x: x_coordinate,
+    bottom_y: y_coordinate,
+    width: x_overlap,
+    height: y_overlap
+    }
+  end
   p overlap
 end
 
-rectangle_1 = {
-  left_x: 1,
-  bottom_y: 1,
-  width: 2,
-  height: 2,
-}
-
-# overlap should return 2, 2, 1, 1
-rectangle_2 = {
-  left_x: 2,
-  bottom_y: 2,
-  width: 2,
-  height: 2,
-}
-
-# no overlap - should return nil
-rectangle_3 = {
-  left_x: 4,
-  bottom_y: 1,
-  width: 1,
-  height: 1,
-}
-
-# touches - should return nil
-rectangle_4 = {
-  left_x: 3,
-  bottom_y: 1,
-  width: 2,
-  height: 2,
-}
-
-# inside - should return 2, 1, 1, 1
-rectangle_5 = {
-  left_x: 2,
-  bottom_y: 1,
-  width: 1,
-  height: 1,
-}
-
-# intersect(rectangle_1, rectangle_2)
-# intersect(rectangle_1, rectangle_3)
