52a7b3e08c1b77473e1ad2b17111e47b9f38aef4
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-10 15:54:44 +0800

    TODO added in ruby file

---
+++ 007_temperaturetracker.rb
@@ -0,0 +1,7 @@
+# TODO:
+# Write a class TempTracker with these methods:
+# 1. insert() - records a new temperature
+# 2. get_max() - returns the highest temp we've seen so far
+# 3. get_min() - returns the lowest temp we've seen so far
+# 4. get_mean() - returns the mean of all temps we've seen so far
+# 5. get_mode() - returns a mode of all temps we've seen so far
