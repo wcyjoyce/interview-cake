d13d2ae0ad0acbffea51ec58cc67ea98b46668a9
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-10 15:47:23 +0800

    reference answer added

--- 006_rectangularlove.rb
+++ 006_rectangularlove.rb
@@ -33,3 +33,37 @@ def intersect(rectangle_1, rectangle_2)
   p overlap
 end
 
+# ANSWER:
+
+def find_range_overlap(point1, length1, point2, length2)
+  highest_start_point = [point1, point2].max
+  lowest_end_point = [point1 + length1, point2 + length2].min
+  return [nil, nil] if highest_start_point >= lowest_end_point
+  # compute the overlap length
+  overlap_length = lowest_end_point - highest_start_point
+  [highest_start_point, overlap_length]
+end
+
+def answer(rect1, rect2)
+  # get the x and y overlap points and lengths
+  x_overlap_point, overlap_width  = find_range_overlap(rect1[:left_x], rect1[:width], rect2[:left_x], rect2[:width])
+  y_overlap_point, overlap_height = find_range_overlap(rect1[:bottom_y], rect1[:height], rect2[:bottom_y], rect2[:height])
+
+  # return nil rectangle if there is no overlap
+  if !overlap_width || !overlap_height
+    answer = {
+      left_x: nil,
+      bottom_y: nil,
+      width: nil,
+      height: nil
+    }
+  else
+    answer = {
+      left_x: x_overlap_point,
+      bottom_y: y_overlap_point,
+      width: overlap_width,
+      height: overlap_height
+    }
+  end
+  p answer
+end
