8330d7f6e28ac6869241b289ec6541f2a2f25abe
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-11-10 16:40:27 +0800

    #007 done

--- 007_temperaturetracker.rb
+++ 007_temperaturetracker.rb
@@ -7,18 +7,39 @@
 # 5. get_mode() - returns a mode of all temps we've seen so far
 
 class TempTracker
-  def insert()
+  def intitialize
+    @occurrences = []
+    @mode = nil
+    @sum = 0
+    @readings = 0
+    @mean = nil
+    @max_temperature = nil
+    @min_temperature = nil
   end
 
-  def get_max()
+  def insert(temperature)
+    @sum += temperature
+    @readings += 1
+    @mean = @sum / @readings
+    @occurrences[temperature] += 1
+    @mode = temperature if @occurrences[temperature].max
+    @max_temperature = temperature if @max_temperature.nil? || temperature > @max_temperature
+    @min_temperature = temperature if @min_temperature.nil? || temperature < @min_temperature
   end
 
-  def get_min()
+  def get_max
+    @max_temperature
   end
 
-  def get_mean()
+  def get_min
+    @min_temperature
   end
 
-  def get_mode()
+  def get_mean
+    @sum / @readings
+  end
+
+  def get_mode
+    @mode
   end
 end
