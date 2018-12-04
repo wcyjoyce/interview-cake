65750d80e0f5b4e3cb905c961e5f0ce3c13f35b5
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-12-05 00:08:36 +0800

    reference answer added

--- 012_findinorderedset.rb
+++ 012_findinorderedset.rb
@@ -9,3 +9,30 @@ end
 search(9, 1..10) # true
 search(9, 1..5) # false
 
+# ANSWER:
+
+## 1) Start with the middle number - is it bigger or smaller than our target number?
+## 2) We've effectively "ruled out" the whole half of the array that we know doesn't contain the target number
+## 3) Repeat the same approach (of starting in the middle) on the new half-size problem
+
+def answer(target, nums)
+  floor_index = -1
+  ceiling_index = nums.length
+
+  while floor_index + 1 < ceiling_index
+    distance = ceiling_index - floor_index
+    half_distance = distance / 2
+    guess_index = floor_index + half_distance
+    guess_value = nums[guess_index]
+
+    p true if guess_value == target
+
+    if guess_value > target
+      ceiling_index = guess_index
+    else
+      floor_index = guess_index
+    end
+  end
+
+  p false
+end
