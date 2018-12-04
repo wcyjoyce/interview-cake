ec73d8f4160f60a49eca85fbe505c5e45a5cbbd1
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-12-04 21:08:20 +0800

    #220 done

--- 220_reversewords.rb
+++ 220_reversewords.rb
@@ -2,3 +2,11 @@
 # Write a method reverse_words() that takes a message as a string and reverses
 # the order of the words in place.
 
+def reverse_words(message)
+  message = message.split(" ").reverse.join(" ")
+  p message
+end
+
+
+reverse_words("the eagle has landed") # landed has eagle the
+reverse_words("cake pound steal") # steal pound cake
