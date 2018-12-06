087dd25cb54e234022be2236533166b41c3a852a
Joyce Wong <wcyjoyce.hk@gmail.com>
2018-12-06 23:04:19 +0800

    reference answer added

--- 217_bracketvalidator.rb
+++ 217_bracketvalidator.rb
@@ -22,3 +22,36 @@ p validate("{ [ ] ( ) }") # true
 p validate("{ [ ( ] ) }") # false
 p validate("{ [ }") # false
 
+# ANSWER:
+
+## 1) If we see an opener, we push it onto the stack
+## 2) If we see a closer, we check to see if it is the closer for the opener at the top of the stack
+## 3) If it is, we pop from the stack; otherwise, or if the stack is empty, we return false
+
+require "set"
+
+def answer(code)
+  openers_to_closers = { "(" => ")", "{" => "}", "[" => "]" }
+
+  openers = openers_to_closers.keys.to_set
+  closers = openers_to_closers.values.to_set
+
+  openers_stack = []
+  code.chars.each do |char|
+    if openers.include?(char)
+      openers_stack.push(char)
+    elsif closers.include?(char)
+      if openers_stack.empty?
+        return false
+      else
+        last_unclosed_opener = openers_stack.pop
+        return false if openers_to_closers[last_unclosed_opener] != char
+      end
+    end
+  end
+  return openers_stack.empty?
+end
+
+# p answer("{ [ ] ( ) }")
+# p answer("{ [ ( ] ) }")
+# p answer("{ [ }")
