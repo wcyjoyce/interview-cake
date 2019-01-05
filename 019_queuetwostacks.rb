7aa36f5287d6c342bdc69e8237694564daf013b4
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-01-05 17:48:46 +0800

    reference answer added

--- 019_queuetwostacks.rb
+++ 019_queuetwostacks.rb
@@ -17,3 +17,27 @@ end
 items = ["a", "b", "c", "d"]
 enqueue(items)
 dequeue(items)
+
+# ANSWER:
+
+class QueueTwoStacks
+  def initialize
+    @in_stack  = []
+    @out_stack = []
+  end
+
+  def enqueue(item)
+    @in_stack.push(item)
+  end
+
+  def dequeue
+    if @out_stack.empty?
+      # Move items from in_stack to out_stack, reversing order
+      until @in_stack.empty?
+        newest = @in_stack.pop
+        @out_stack.push(newest)
+      end
+    end
+    @out_stack.empty? ? nil : @out_stack.pop
+  end
+end
