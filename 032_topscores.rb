83c10ac72ad63686d1460cf173bfe110e0795130
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-01-07 13:29:27 +0800

    #32 done

--- 032_topscores.rb
+++ 032_topscores.rb
@@ -2,3 +2,16 @@
 # Write a method that takes (1) an array of unsorted_scores and (2) the
 # highest_possible_score in the game and returns a sorted array of scores
 # efficiently.
+
+def sort_scores(unsorted_scores, highest_possible_score)
+  array = []
+  array << highest_possible_score
+  unsorted_scores.sort.reverse.each { |score| array << score }
+end
+
+unsorted_scores = [37, 89, 41, 65, 91, 53]
+highest_possible_score = 100
+
+p sort_scores(unsorted_scores, highest_possible_score)
+# returns [91, 89, 65, 53, 41, 37]
+
